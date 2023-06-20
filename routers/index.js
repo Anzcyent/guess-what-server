fa7024607137d6d6fb5b770acfcc2821ef9e7d96
Cihan <ozcancihan714@gmail.com>
2023-06-20 12:22:45 +0300

    test has been removed

--- routers/index.js
+++ routers/index.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {getAccessToRoute} = require("../middlewares/auth/auth");
 const auth = require("./auth");
 const game = require("./game");
-const test = require("./test");
 
 router.use("/auth", auth);
 router.use("/game", game);
-
-router.use("/test", getAccessToRoute, test);
 
 module.exports = router;
